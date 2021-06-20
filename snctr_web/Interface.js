71298f2e5ec07832439f1f0fb7c4489387cde0c7
codeanticode <andres.colubri@gmail.com>
2021-06-20 15:55:35 -0400

    compartir url con web share api y clipboard como fallback

--- snctr_web/Interface.js
+++ snctr_web/Interface.js
@@ -3,24 +3,39 @@ var Interface = function() {
 
     // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share
     // https://css-tricks.com/how-to-use-the-web-share-api/
-          const shareData = {
-            title: 'SINCENTRO',
-            text: 'Dibujemos juntos',
-            url: 'file:///Users/andres/code/sincentro/snctr_web/index.html?peer=' + this.peerID,
-          }    
+
     
           createButton('+').position(width - 50, 0).size(30, 30).mousePressed(async () => {
+            const urlText =  'file:///Users/andres/code/sincentro/snctr_web/index.html?peer=' + miID;
+            print("======>", urlText);
+  
+            const shareData = {
+              title: 'SINCENTRO',
+              text: urlText,
+            //   url: urlText,
+            }
+
             if (navigator.share) {
               try {
                 await navigator.share(shareData)
-                print('MDN shared successfully');
+                print('url shared successfully');
                 // resultPara.textContent = 'MDN shared successfully'
               } catch(err) {
                 print('Error: ' + err);
                 // resultPara.textContent = 'Error: ' + err
               }                
+            } else if (navigator.clipboard) {
+                // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+                print('Share API not available in this browser, will use clipboard');
+                navigator.clipboard.writeText(urlText).then(function() {
+                    /* clipboard successfully set */
+                    print('Copied to clipboard');
+                    mensajes.agregar("URL COPIADO AL PORTAPAPELES")
+                  }, function() {
+                    /* clipboard write failed */
+                  });
             } else {
-                print('Share API not available in this browser');
+                print('cannot share');
             }
 
           });
