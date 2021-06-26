d1057ee189b0907bc06d3210aeece0821c66c14e
codeanticode <andres.colubri@gmail.com>
2021-06-25 23:14:09 -0400

    agregado soporte de presion

--- snctr_web/sincentro.js
+++ snctr_web/sincentro.js
@@ -7,6 +7,9 @@ var estado;
 var mensajes;
 var iu;
 
+var presionInicializada = false;
+var presion = -2;
+
 function setup() {
   configurarPantallaCompleta();
   disableScroll();
@@ -33,6 +36,11 @@ function setup() {
 
 function draw() {
   if (mostrandoPortada()) return
+
+  if (!presionInicializada) {
+    inicializarPresion();
+  }
+
   actualizarEstados();
   lienzo.pintar();
   pintarCapas();
@@ -43,13 +51,14 @@ function draw() {
 
 function mousePressed() {
   if (mostrandoID || mostrandoPortada()) return
-  estado.iniciarTrazo(estado.indiceTrazo + 1, mouseX, mouseY, sqrt(sq(mouseX - pmouseX) + sq(mouseY - pmouseY)), millis(), true);
+  estado.iniciarTrazo(estado.indiceTrazo + 1, mouseX, mouseY, presion, millis(), true);
   return false;
 }
 
 function mouseDragged() {
   if (mostrandoID || mostrandoPortada()) return
-  estado.actualizarTrazo(estado.indiceTrazo, mouseX, mouseY, sqrt(sq(mouseX - pmouseX) + sq(mouseY - pmouseY)), millis(), true);
+  print(presion);
+  estado.actualizarTrazo(estado.indiceTrazo, mouseX, mouseY, presion, millis(), true);
   return false;
 }
 
@@ -84,7 +93,9 @@ function configurarPantallaCompleta() {
     h = document.body.clientHeight;
   }
   var canvas = createCanvas(w, h);
-  canvas.parent('sincentro'); 
+  canvas.parent('sincentro');
+  canvas.id("sincentroCanvas");
+  canvas.position(0, 0);
 }
 
 // Disabling scrolling and bouncing on iOS Safari
@@ -96,4 +107,43 @@ function preventDefault(e){
 
 function disableScroll(){
   document.body.addEventListener('touchmove', preventDefault, { passive: false });
+}
+
+// Initializing Pressure.js
+// https://pressurejs.com/documentation.html
+function inicializarPresion() {
+
+  //console.log("Attempting to initialize Pressure.js ");
+
+  Pressure.set('#sincentroCanvas', {
+
+    start: function(event){
+      // this is called on force start
+      // isDrawing = true;
+      // isDrawingJustStarted = true;
+    },
+    end: function(){
+      // this is called on force end
+      // isDrawing = false
+      presion = 0;
+    },
+    change: function(force, event) {
+      if (!presionInicializada){
+        console.log("Pressure.js initialized successfully");
+        presionInicializada = true;
+      }
+      //console.log(force);
+      presion = force;
+
+    }
+  });
+
+  Pressure.config({
+    polyfill: true, // use time-based fallback ?
+    polyfillSpeedUp: 1000, // how long does the fallback take to reach full pressure
+    polyfillSpeedDown: 300,
+    preventSelect: true,
+    only: null
+    });
+
 }
