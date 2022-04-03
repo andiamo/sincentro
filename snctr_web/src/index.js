1c0444d37f2814272c69047546deb88312cc2b86
codeanticode <andres.colubri@gmail.com>
2022-04-02 22:49:09 -0400

    utilizanr clase sketch

--- snctr_web/src/index.js
+++ snctr_web/src/index.js
@@ -1,118 +1,121 @@
-var pinceles = [];
-var tintasFondo = [];
-var tintasPincel = [];
-var capas = [];
-var lienzo;
-var estado;
-var mensajes;
-var iu;
+const filters = new Filters();
 
-var presionInicializada = false;
-var presion = -2;
-var presionEscala = 5;
-
-function setup() {
-  configurarPantallaCompleta();
-  disableScroll();
-  cursor(CROSS);
+var sketch = function(p) {
+  var pinceles = [];
+  var tintasFondo = [];
+  var tintasPincel = [];
+  var capas = [];
+  var lienzo;
+  var estado;
+  var mensajes;
+  var iu;
   
-  // https://www.codeleaks.io/get-url-parameter-javascript/
-  // https://developer.mozilla.org/en-US/docs/Web/API/Location/search
-  this.queryString = window.location.search;
-  const urlParams = new URLSearchParams(this.queryString);
-  const otroID = urlParams.get('peer')
-
-  cargarPinceles();  
-  cargarColores();  
-  crearCapas();
-
-  lienzo = new LienzoFondo();
-  estado = new Estado();
-  mensajes = new Mensajes();
-  iu = new Interface();
-
-  iniciarP2P(otroID);
-
-  // mostrarPortada();
-}
-
-function draw() {
-  if (mostrandoPortada()) return
-
-  if (!presionInicializada) {
-    inicializarPresion();
+  var presionInicializada = false;
+  var presion = -2;
+  var presionEscala = 5;
+  
+  p.preload = function() {
   }
 
-  actualizarEstados();
-  lienzo.pintar();
-  pintarCapas();
-  estado.mostrar();
-  mensajes.mostrar();
-  iu.mostrar();
-}
-
-function mousePressed() {
-  if (mostrandoID || mostrandoPortada()) return
-  estado.iniciarTrazo(estado.indiceTrazo + 1, mouseX, mouseY, presion, millis(), true);
-  return false;
-}
-
-function mouseDragged() {
-  if (mostrandoID || mostrandoPortada()) return
-  estado.actualizarTrazo(estado.indiceTrazo, mouseX, mouseY, presion, millis(), true);
-  return false;
-}
-
-function mouseReleased() {
-  if (mostrandoID || mostrandoPortada()) return
-  estado.terminarTrazo(estado.indiceTrazo, modificador() === SHIFT, true);
-  return false;
-}
-
-function keyPressed() {
-  if (mostrandoPortada()) return false
-  estado.procesarTeclado(keyCode, key, true);
-  return false;
-}
-
-function configurarPantallaCompleta() {
-  let w = 0, h = 0;
-  if(typeof(window.innerWidth) === 'number') {
-    // Non-IE
-    w = window.innerWidth;
-    h = window.innerHeight;
-  } else if(document.documentElement && 
-  	        (document.documentElement.clientWidth || 
-  	         document.documentElement.clientHeight)) {
-    // IE 6+ in 'standards compliant mode'
-    w = document.documentElement.clientWidth;
-    h = document.documentElement.clientHeight;
-  } else if(document.body && (document.body.clientWidth || 
-  	                          document.body.clientHeight)) {
-    // IE 4 compatible
-    w = document.body.clientWidth;
-    h = document.body.clientHeight;
+  p.setup = function() {
+    configurarPantallaCompleta();
+    disableScroll();
+    cursor(CROSS);
+    
+    // https://www.codeleaks.io/get-url-parameter-javascript/
+    // https://developer.mozilla.org/en-US/docs/Web/API/Location/search
+    this.queryString = window.location.search;
+    const urlParams = new URLSearchParams(this.queryString);
+    const otroID = urlParams.get('peer')
+  
+    cargarPinceles();  
+    cargarColores();  
+    crearCapas();
+  
+    lienzo = new LienzoFondo();
+    estado = new Estado();
+    mensajes = new Mensajes();
+    iu = new Interface();
+  
+    iniciarP2P(otroID);
+  
+    // mostrarPortada();    
   }
-  var canvas = createCanvas(w, h);
-  canvas.parent('sincentro');
-  canvas.id("sincentroCanvas");
-  canvas.position(0, 0);
-}
+  
+  p.draw = function() {
+    if (mostrandoPortada()) return
+
+    if (!presionInicializada) {
+      inicializarPresion();
+    }
+  
+    actualizarEstados();
+    lienzo.pintar();
+    pintarCapas();
+    estado.mostrar();
+    mensajes.mostrar();
+    iu.mostrar();    
+  }
+
+  p.mousePressed = function() {
+    if (mostrandoID || mostrandoPortada()) return
+    estado.iniciarTrazo(estado.indiceTrazo + 1, mouseX, mouseY, presion, millis(), true);
+    return false;    
+  }  
+  p.mouseDragged = function() {
+    if (mostrandoID || mostrandoPortada()) return
+    estado.actualizarTrazo(estado.indiceTrazo, mouseX, mouseY, presion, millis(), true);
+    return false;    
+  }
+  p.mouseReleased = function() {
+    if (mostrandoID || mostrandoPortada()) return
+    estado.terminarTrazo(estado.indiceTrazo, modificador() === SHIFT, true);
+    return false;    
+  }
+  p.keyPressed = function() {
+    if (mostrandoPortada()) return false
+    estado.procesarTeclado(keyCode, key, true);
+    return false;    
+  }
+
+  p.configurarPantallaCompleta = function() {
+    let w = 0, h = 0;
+    if(typeof(window.innerWidth) === 'number') {
+      // Non-IE
+      w = window.innerWidth;
+      h = window.innerHeight;
+    } else if(document.documentElement && 
+              (document.documentElement.clientWidth || 
+               document.documentElement.clientHeight)) {
+      // IE 6+ in 'standards compliant mode'
+      w = document.documentElement.clientWidth;
+      h = document.documentElement.clientHeight;
+    } else if(document.body && (document.body.clientWidth || 
+                                document.body.clientHeight)) {
+      // IE 4 compatible
+      w = document.body.clientWidth;
+      h = document.body.clientHeight;
+    }
+    var canvas = createCanvas(w, h);
+    canvas.parent('sincentro');
+    canvas.id("sincentroCanvas");
+    canvas.position(0, 0);    
+  }
 
 // Disabling scrolling and bouncing on iOS Safari
 // https://stackoverflow.com/questions/7768269/ipad-safari-disable-scrolling-and-bounce-effect
 
-function preventDefault(e){
+p.preventDefault = function(e) {
   e.preventDefault();
 }
 
-function disableScroll(){
+p.disableScroll = function() {
   document.body.addEventListener('touchmove', preventDefault, { passive: false });
 }
 
 // Initializing Pressure.js
 // https://pressurejs.com/documentation.html
-function inicializarPresion() {
+p.inicializarPresion = function() {
 
   //console.log("Attempting to initialize Pressure.js ");
 
@@ -147,4 +150,5 @@ function inicializarPresion() {
     only: null
     });
 
+  }  
 }
